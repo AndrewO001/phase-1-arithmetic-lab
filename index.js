a97ddecab7b50942c5f998609c1d491fd77fada5
Andrew Di Jorio <acdijorio@gmail.com>
2023-11-02 00:24:51 -0500

    code completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4;
+
+let max = Math.max(1, 10, 20);
